161e59d1854083ab27f0a4dd17b4d7302c38a752
JORGE <jariasr2@ucenfotec.ac.cr>
2022-04-29 22:19:05 -0600

    edicionUsuarios

--- ProyectoFinal/api/editarUsuarios.js
+++ ProyectoFinal/api/editarUsuarios.js
@@ -2,10 +2,10 @@ var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
 
-var libro = require('../schemas/editarUsuario.js');
+var editarUsuario = require('../schemas/editarUsuario.js');
 
 router.get('/', function(req, res) {
-  libro.find().exec()
+  editarUsuario.find().exec()
     .then(
       function(result) {
         res.json(result);
@@ -15,7 +15,7 @@ router.get('/', function(req, res) {
 
 router.post('/buscar', function(req, res) {
   var idUsuario = req.body.idUsuario;
-  libro.findById(idUsuario).exec()
+  editarUsuario.findById(idUsuario).exec()
     .then(
       function(result) {
         res.json(result);
@@ -25,42 +25,34 @@ router.post('/buscar', function(req, res) {
 
 router.post('/actualizar', function(req, res) {
   var nombre = req.body.nombre;
-  var precio = req.body.precio;
-  var disponibilidad = req.body.disponibilidad;
-  var fechaDePublicacion = req.body.fechaDePublicacion;
-  var formato = req.body.formato;
-  var genero = req.body.genero;
-  var resenna = req.body.resenna;
-  var ism = req.body.ism;
-  var editorial = req.body.editorial;
-  var premiosEnHonor = req.body.premiosEnHonor;
-
+  var apellido = req.body.apellido;
+  var fechaDeNacimiento = req.body.fechaDeNacimiento;
+  var correoElectronico = req.body.correoElectronico;
+  var contrasenna = req.body.contrasenna;
+  var puntaje = req.body.puntaje;
+  
+  
   // findOneAndUpdate - Filtro - Valores - Opciones - Función anónima
-  libro.findOneAndUpdate({nombre: nombre},{$set:{precio:precio, disponibilidad:disponibilidad, fechaDePublicacion:fechaDePublicacion, formato:formato, genero:genero, resenna:resenna, ism:ism, editorial:editorial, premiosEnHonor:premiosEnHonor}}, {useFindAndModify: false, new: true}, function (err, doc) {
+  editarUsuario.findOneAndUpdate({nombre: nombre},{$set:{apellido:apellido, fechaDeNacimiento:fechaDeNacimiento, correoElectronico:correoElectronico, contrasenna:contrasenna, puntaje:puntaje}}, {useFindAndModify: false, new: true}, function (err, doc) {
     res.json(doc);
   });
 });
 
 router.post('/insertar', function(req, res) {
-  var libroNuevo = new libro({
+  var editarUsuarioNuevo = new editarUsuario({
 
     _id: new mongoose.Types.ObjectId(),
     nombre: req.body.nombre,
-    disponibilidad: req.body.disponibilidad,
-    fechaDePublicacion: req.body.fechaDePublicacion,
-    formato: req.body.formato,
-    genero: req.body.genero,
-    precio: req.body.precio,
-    resenna: req.body.resenna,
-    imagen: req.body.imagen,
-    ism: req.body.ism,
-    editorial: req.body.editorial,
-    premiosEnHonor: req.body.premiosEnHonor,
-    idAutor: req.body.idAutor
+    apellido: req.body.apellido,
+    fechaDeNacimiento: req.body.fechaDeNacimiento,
+    correoElectronico: req.body.correoElectronico,
+    contrasenna: req.body.contrasenna,
+    puntaje: req.body.puntaje,
+    
 
   });
 
-  libroNuevo.save()
+  editarUsuarioNuevo.save()
     .then(
       function(result) {
         res.json(result);
