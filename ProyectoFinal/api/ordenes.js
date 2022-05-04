182023a9370fec8a9400de6e48ce8e962b2ae5cd
David <jhondrek116@gmail.com>
2022-05-04 15:48:05 -0600

    Catalogo Ordenes

--- ProyectoFinal/api/ordenes.js
+++ ProyectoFinal/api/ordenes.js
@@ -24,83 +24,33 @@ router.post('/buscar', function(req, res) {
 });
 
 
-//FUNCION PARA BUSCAR POR NOMBRE DE LIBRO
-router.post('/buscarNombre', function(req, res) {
-  var nombre = req.body.nombre;
-  
-  orden.find({ nombre:{'$regex' : nombre, '$options' : 'i'}}).exec()
-    .then(
-      function(result) {
-        res.json(result);
-      }
-    );
-});
-
-
-router.post('/buscarGenero', function(req, res) {
-  var genero = req.body.genero;
-  
-  orden.find({ genero:{'$regex' : genero, '$options' : 'i'}}).exec()
-    .then(
-      function(result) {
-        res.json(result);
-      }
-    );
-});
-
-
-router.post('/buscarAutor', function(req, res) {
-  var nombreAutor = req.body.nombreAutor;
-  
-  orden.find({ nombreAutor:{'$regex' : nombreAutor, '$options' : 'i'}}).exec()
-    .then(
-      function(result) {
-        res.json(result);
-      }
-    );
-});
-
 
 
 router.post('/actualizar', function(req, res) {
-  var nombre = req.body.nombre;
-  var precio = req.body.precio;
-  var disponibilidad = req.body.disponibilidad;
-  var fechaDePublicacion = req.body.fechaDePublicacion;
-  var formato = req.body.formato;
-  var genero = req.body.genero;
-  var resenna = req.body.resenna;
-  var ism = req.body.ism;
-  var editorial = req.body.editorial;
-  var premiosEnHonor = req.body.premiosEnHonor;
+  var idUsuario = req.body.idUsuario;
+  var idLibro = req.body.idLibro;
+  var idPago = req.body.idPago;
+  var cantidadLibros = req.body.cantidadLibros;
+ 
 
   // findOneAndUpdate - Filtro - Valores - Opciones - Función anónima
-  orden.findOneAndUpdate({nombre: nombre},{$set:{precio:precio, disponibilidad:disponibilidad, fechaDePublicacion:fechaDePublicacion, formato:formato, genero:genero, resenna:resenna, ism:ism, editorial:editorial, premiosEnHonor:premiosEnHonor}}, {useFindAndModify: false, new: true}, function (err, doc) {
+  orden.findOneAndUpdate({idUsuario: idUsuario},{$set:{idLibro:idLibro, idPago:idPago, cantidadLibros:cantidadLibros}}, {useFindAndModify: false, new: true}, function (err, doc) {
     res.json(doc);
   });
 });
 
 router.post('/insertar', function(req, res) {
-  var libroNuevo = new orden({
+  var ordenNueva = new orden({
 
     _id: new mongoose.Types.ObjectId(),
-    nombre: req.body.nombre,
-    disponibilidad: req.body.disponibilidad,
-    fechaDePublicacion: req.body.fechaDePublicacion,
-    formato: req.body.formato,
-    genero: req.body.genero,
-    precio: req.body.precio,
-    resenna: req.body.resenna,
-    imagen: req.body.imagen,
-    ism: req.body.ism,
-    editorial: req.body.editorial,
-    premiosEnHonor: req.body.premiosEnHonor,
-    nombreAutor: req.body.nombreAutor,
-    idAutor: req.body.idAutor
-
+    idUsuario: req.body.idUsuario,
+    idLibro: req.body.idLibro,
+    idPago: req.body.idPago,
+    cantidadLibros: req.body.cantidadLibros
+    
   });
 
-  libroNuevo.save()
+  ordenNueva.save()
     .then(
       function(result) {
         res.json(result);
